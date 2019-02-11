299a2c4d72ff79446ac4f26827463ed0b13bf087
James Talton <James.Talton@ibm.com>
2019-02-11 15:01:38 -0500

    fix(export): export setLogger

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { startCluster } from './clustered'
 export { createAppServer, shutdownAppServer } from './server'
-export { ILogger, ILogObject, logger, setLogLevel, LogLevel } from './logger'
+export { ILogger, ILogObject, logger, setLogger, setLogLevel, LogLevel } from './logger'
 export { consoleLogger } from './console-logger'
