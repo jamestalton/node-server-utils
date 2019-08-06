ad52c1c71bcfa7a2f483ece028940b567aaeb9cd
Travis CI <travis@travis-ci.org>
2019-08-06 16:50:43 -0400

    build tweaks

---
+++ scripts/publish.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x
+set -e
+
+PUBLISHED_VERSION=`npm view node-server-utils version`
+npm version --no-git-tag-version $PUBLISHED_VERSION
+PUBLISHED_SHA=`npm view node-server-utils --json | jq .dist.shasum`
+NEW_SHA=`npm publish --dry-run --json | jq .shasum`
+if [ "$PUBLISHED_SHA" != "$NEW_SHA" ]; then 
+    npm version patch --no-git-tag-version
+    npm publish
+fi
