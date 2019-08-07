4f245453c4b1f773d33c5560dc445976bf2850c3
Travis CI <travis@travis-ci.org>
2019-08-07 13:39:13 -0400

    Robust scripts

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,33 +2,33 @@
 set -x
 set -e
 
-if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
-  if [ "$GH_TOKEN" = "" ]; then
-    echo "GH_TOKEN required"
-    exit 1
-  fi
+if [ "$GH_TOKEN" = "" ]; then
+  echo "GH_TOKEN required"
+  exit 1
+fi
 
-  git remote remove origin
-  git remote add origin https://${GH_TOKEN}@github.com/jamestalton/node-server-utils.git > /dev/null 2>&1
-  git checkout master
+PACKAGE_NAME=`cat package.json | jq .name | tr -d '"'`
 
-  rm -rf node_modules
-  rm -f package-lock.json
+git remote remove origin
+git remote add origin https://${GH_TOKEN}@github.com/jamestalton/${PACKAGE_NAME}.git > /dev/null 2>&1
+git checkout master
 
-  npx npm-check-updates -u
-  npm install
-  npm audit fix
+rm -rf node_modules
+rm -f package-lock.json
 
-  if ! git diff-files --quiet --ignore-submodules -- > /dev/null; then
-    npm build
-    npm test
-    npm audit
-    git config --global user.email "travis@travis-ci.org"
-    git config --global user.name "Travis CI"
-    git add -u :/
-    git commit -m "fix(deps): upgrade dependencies"
-    git push origin master
-  else
-    echo No upgrades available
-  fi
+npx npm-check-updates -u
+npm install
+npm audit fix
+
+if ! git diff-files --quiet --ignore-submodules -- > /dev/null; then
+  npm build
+  npm test
+  npm audit
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+  git add -u :/
+  git commit -m "fix(deps): upgrade dependencies"
+  git push origin master
+else
+  echo No upgrades available
 fi
